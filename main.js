b81fc17abcd85237d6c6baf85057e4af28feea75
Lauren Carroll-Allan <lcarrollallan@gmail.com>
2018-02-18 14:51:50 -0800

    added features 5 and 6

--- main.js
+++ main.js
@@ -1,69 +1,130 @@
-var name = '';
-var minCust = 0;
-var maxCust = 0;
-var avgSale = 0;
-var hours = ['10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm'];
+var storeName = '';
+var minCustomer = 0;
+var maxCustomer = 0;
+var avgCookiesPerCustomer = 0;
+var hoursOpen = ['10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm'];
 
-function getRandomCustomer(minCust, maxCust) {
-  return Math.floor(Math.random() * (maxCust - minCust + 1)) + minCust;
+//2. Use a method to generate a random number of customers per hour.
+//3. Calculate simulated amounts of cookies purchased for each hour using average cookies purchased and the random number of customers generated.
+function getCookiesPerHour(minCustomer, maxCustomer, avgCookiesPerCustomer) {
+  return ((Math.floor(Math.random() * (maxCustomer - minCustomer + 1)) + minCustomer) * avgCookiesPerCustomer);
 }
 
+//1. Create objects that store the min/max hourly customers, and the average cookies per customer, in object properties
 var pioneer = {
-  name: 'Pioneer Square',
-  minCust: 17,
-  maxCust: 58,
-  avgSale: 5.2,
+  storeName: 'Pioneer Square',
+  minCustomer: 17,
+  maxCustomer: 58,
+  avgCookiesPerCustomer: 5.2,
+  totalCookies: 0,
 
-  getCookiesPerHour: function() {
-    for (var i = 0; i < hours.length; i++) {
-      this.allCookiesPerHour.push(
-        Math.round(getRandomCustomer(this.minCust, this.maxCust) * this.avgSale)
-      )
+  getAllCookies: function() {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      var cookies = Math.round(getCookiesPerHour(this.minCustomer, this.maxCustomer, this.avgCookiesPerCustomer));
+      this.allCookiesPerHour.push(cookies);
+      this.totalCookies += cookies;
     }
   },
-
+//4. Store the results for each location in a separate array
   allCookiesPerHour: []
-
 }
 
-
-
 var airport = {
-  name: 'Portland Airport',
-  minCust: 6,
-  maxCust: 24,
-  avgSale: 1.2,
-  getCookiesPerHour: function() {
-    return getRandomCustomer(this.minCust, this.maxCust) * this.avgSale;
-  }
+  storeName: 'Portland Airport',
+  minCustomer: 6,
+  maxCustomer: 24,
+  avgCookiesPerCustomer: 1.2,
+  totalCookies: 0,
+
+  getAllCookies: function() {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      var cookies = Math.round(getCookiesPerHour(this.minCustomer, this.maxCustomer, this.avgCookiesPerCustomer));
+      this.allCookiesPerHour.push(cookies);
+      this.totalCookies += cookies;
+    }
+  },
+//4. Store the results for each location in a separate array
+  allCookiesPerHour: []
 }
 
 var washington = {
-  name: 'Washington Square',
-  minCust: 11,
-  maxCust: 38,
-  avgSale: 1.9,
-  getCookiesPerHour: function() {
-    return getRandomCustomer(this.minCust, this.maxCust) * this.avgSale;
-  }
+  storeName: 'Washington Square',
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCookiesPerCustomer: 1.9,
+  totalCookies: 0,
+
+  getAllCookies: function() {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      var cookies = Math.round(getCookiesPerHour(this.minCustomer, this.maxCustomer, this.avgCookiesPerCustomer));
+      this.allCookiesPerHour.push(cookies);
+      this.totalCookies += cookies;
+    }
+  },
+//4. Store the results for each location in a separate array
+  allCookiesPerHour: []
 }
 
 var sellwood = {
-  name: 'Sellwood',
-  minCust: 17,
-  maxCust: 48,
-  avgSale: 3.3,
-  getCookiesPerHour: function() {
-    return getRandomCustomer(this.minCust, this.maxCust) * this.avgSale;
-  }
+  storeName: 'Sellwood',
+  minCustomer: 17,
+  maxCustomer: 48,
+  avgCookiesPerCustomer: 3.3,
+  totalCookies: 0,
+
+  getAllCookies: function() {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      var cookies = Math.round(getCookiesPerHour(this.minCustomer, this.maxCustomer, this.avgCookiesPerCustomer));
+      this.allCookiesPerHour.push(cookies);
+      this.totalCookies += cookies;
+    }
+  },
+//4. Store the results for each location in a separate array
+  allCookiesPerHour: []
 }
 
 var pearl = {
-  name: 'Pearl District',
-  minCust: 3,
-  maxCust: 24,
-  avgSale: 2.6,
-  getCookiesPerHour: function() {
-    return getRandomCustomer(this.minCust, this.maxCust) * this.avgSale;
-  }
+  storeName: 'Pearl District',
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookiesPerCustomer: 2.6,
+  totalCookies: 0,
+
+  getAllCookies: function() {
+    for (var i = 0; i < hoursOpen.length; i++) {
+      var cookies = Math.round(getCookiesPerHour(this.minCustomer, this.maxCustomer, this.avgCookiesPerCustomer));
+      this.allCookiesPerHour.push(cookies);
+      this.totalCookies += cookies;
+    }
+  },
+//4. Store the results for each location in a separate array
+  allCookiesPerHour: []
 }
+
+//5. Display the values of each array as unordered lists in the browser
+//6. Calculate the sum of the hourly totals.
+var stores = [pioneer, airport, washington, sellwood, pearl];
+
+var storeContainer = document.getElementById('store-container');
+for (var storeIndex = 0; storeIndex < stores.length; storeIndex++) {
+  var store = stores[storeIndex];
+  store.getAllCookies();
+  var storeList = '<ul><h3>' + store.storeName + '</h3>';
+  for(var hourIndex = 0; hourIndex < hoursOpen.length; hourIndex ++) {
+    storeList += '<li>' + hoursOpen[hourIndex] + ': ' + store.allCookiesPerHour[hourIndex] + '</li>';
+  }
+  storeList += '<li>' + 'TOTAL: ' + store.totalCookies + '</li>';
+  storeContainer.innerHTML += storeList + '</ul>';
+}
+
+
+
+/*
+var storeContainer = document.getElementById('pioneer');
+var storeList = '<h3>' + pioneer.storeName + '</h3>';
+pioneer.getAllCookies();
+for (var i = 0; i < hoursOpen.length; i++) {
+  storeList += '<li>' + hoursOpen[i] + ': ' + pioneer.allCookiesPerHour[i] + '</li>';
+}
+storeContainer.innerHTML = storeList;
+*/
